0eca40e2a95504986cc540bf1260e5f42ecb3ace
yves <yves@laptop-yves>
2016-04-28 00:41:13 +0200

    Adding characters management system

--- findpassword.py
+++ findpassword.py
@@ -8,7 +8,7 @@ chars_ups = [ 'A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Q', 'S', 'D', '
 chars_digits = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 chars_specials = [ ',', '?', ';', '.', ':', '/', '!', '%', '*',  '$', '"', '\'', '(', '-', '_', ')', '=', '}', ']', '@', '^', '\\', '`', '|', '[', '{', '~' ]
 
-chars = chars_lows + chars_ups + chars_digits + chars_specials
+chars = []
 
 # Checks a single password and tell if it's correct or not
 def test(url, string, password):
@@ -16,6 +16,23 @@ def test(url, string, password):
 	response = urllib2.urlopen(query)
 	content = response.read()
 	return string in content
+
+# Prepares the characters used for bruteforcing password
+def prepare(order):
+	global chars
+	for char in order:
+		if char == 'L':
+			chars = chars + chars_lows
+		elif char == 'U':
+			chars = chars + chars_ups
+		elif char == 'D':
+			chars = chars + chars_digits
+		elif char == 'S':
+			chars = chars + chars_specials
+		else:
+			print "Invalid character order : {}".format(char)
+			return False
+	return True
 
 # Loop bruteforcing each character of password
 def find(url, string, seed):
@@ -33,6 +50,7 @@ parser = OptionParser()
 parser.add_option("-u", dest="url", help="URL with payload. Replace password with {}")
 parser.add_option("-s", dest="string", help="String to look for in response to check whether tested password fragment is correct")
 parser.add_option("--seed", dest="seed", help="Starting password fragment. Will start with an empty password if this option isn't provided", default="")
+parser.add_option("--chars", dest="characters", help="Characters pool and order ((L)ow, (U)p, (D)igits, (S)pecials). Default to DLUS if this options isn't provided", default="DLUS")
 (options, args) = parser.parse_args()
 
 if not options.url:
@@ -41,4 +59,5 @@ if not options.string:
 	parser.error("No string provided")
 
 # Go
-find(options.url, options.string, options.seed)
+if prepare(options.characters):
+	find(options.url, options.string, options.seed)
