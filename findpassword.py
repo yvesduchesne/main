6c465c7fd171cbca797439f505cc6d529940dee1
yves <yves@laptop-yves>
2016-04-28 14:43:12 +0200

    Adding verbosity

--- findpassword.py
+++ findpassword.py
@@ -9,6 +9,7 @@ chars_digits = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 chars_specials = [ ',', '?', ';', '.', ':', '/', '!', '%', '*',  '$', '"', '\'', '(', '-', '_', ')', '=', '}', ']', '@', '^', '\\', '`', '|', '[', '{', '~' ]
 
 chars = []
+verbose = False
 
 # Checks a single password and tell if it's correct or not
 def test(url, string, password):
@@ -19,15 +20,20 @@ def test(url, string, password):
 
 # Prepares the characters used for bruteforcing password
 def prepare(order):
+	debug("Preparing characters...")
 	global chars
 	for char in order:
 		if char == 'L':
+			debug("Adding lower case (a-z)")
 			chars = chars + chars_lows
 		elif char == 'U':
+			debug("Adding upper case (A-Z)")
 			chars = chars + chars_ups
 		elif char == 'D':
+			debug("Adding digits (0-9)")
 			chars = chars + chars_digits
 		elif char == 'S':
+			debug("Adding special characters (',', '?', ';', '.', ':', '/', '!', '%', '*',  '$', '\"', ''', '(', '-', '_', ')', '=', '}', ']', '@', '^', '\\', '`', '|', '[', '{', '~'")
 			chars = chars + chars_specials
 		else:
 			print "Invalid character order : {}".format(char)
@@ -38,17 +44,24 @@ def prepare(order):
 def find(url, string, seed):
 	for char in chars:
 		newseed = seed + char
+		debug("* Testing character {}. Password is {}".format(char, newseed))
 		if test(url, string, newseed):
-			print "* {}".format(newseed)
+			print("* Character found : {}, password is {}".format(char, newseed))
 			find(url, string, newseed)
 			return
 	print "* ---"
 	print "* Password found : {}".format(seed)
 
+def debug(string):
+	global verbose
+	if verbose:
+		print string
+
 # Options parser
 parser = OptionParser()
 parser.add_option("-u", dest="url", help="URL with payload. Replace password with {}")
 parser.add_option("-s", dest="string", help="String to look for in response to check whether tested password fragment is correct")
+parser.add_option("-v", dest="verbose", action="store_true", help="Verbose mode", default=False)
 parser.add_option("--seed", dest="seed", help="Starting password fragment. Will start with an empty password if this option isn't provided", default="")
 parser.add_option("--chars", dest="characters", help="Characters pool and order ((L)ow, (U)p, (D)igits, (S)pecials). Default to DLUS if this options isn't provided", default="DLUS")
 (options, args) = parser.parse_args()
@@ -57,6 +70,7 @@ if not options.url:
 	parser.error("No URL provided")
 if not options.string:
 	parser.error("No string provided")
+verbose = options.verbose
 
 # Go
 if prepare(options.characters):
